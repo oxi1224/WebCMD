0a2939d7733bb1301db49d07e3bee3283a275a7e
ox-i <oxi1224@o2.pl>
2024-02-11 15:03:52 +0100

    Added package.bat

---
+++ package.bat
@@ -0,0 +1,9 @@
+rmdir release /S /Q
+mkdir release
+xcopy "static" "release/static" /E /I /Y
+copy ".env.exmpl" "release/.env"
+copy "out\build\x64-Release\WebCMD.exe" "release/WebCMD.exe"
+cd "release"
+tar.exe -acvf WebCMD.zip  --exclude=*.zip *
+cd ".."
+move "release\WebCMD.zip" "./"
